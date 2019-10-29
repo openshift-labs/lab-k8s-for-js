576769c812a422da4ace82bd8ed07b5f80b79c36
jankleinert <jkleiner@redhat.com>
2019-10-29 16:33:08 -0400

    Add submodule, update settings

--- .workshop/settings.sh
+++ .workshop/settings.sh
@@ -1,7 +1,7 @@
-WORKSHOP_NAME=lab-markdown-sample
-WORKSHOP_IMAGE=quay.io/openshifthomeroom/lab-markdown-sample:1.9
-WORKSHOP_TITLE="Markdown Sample"
-WORKSHOP_DESCRIPTION="A sample workshop using Markdown"
-RESOURCE_BUDGET=medium
-MAX_SESSION_AGE=3600
-IDLE_TIMEOUT=300
+WORKSHOP_NAME=lab-k8s-for-js
+WORKSHOP_IMAGE=quay.io/repository/openshiftlabs/lab-k8s-for-js
+WORKSHOP_TITLE="Kubernetes and OpenShift for JS Developers"
+WORKSHOP_DESCRIPTION="A hands-on introduction to Kubernetes and OpenShift for JS Developers"
+RESOURCE_BUDGET=large
+MAX_SESSION_AGE=9600
+IDLE_TIMEOUT=600
